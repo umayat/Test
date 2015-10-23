31b9cbef4fc044e4949ba7ac833390b2c4a96628
umayat <umayat@requiem.ee.ryerson.ca>
2015-10-23 16:37:37 -0400

    Added new line.

---
+++ Test/src/Test.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : Uzair
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Test" << endl; // prints
+	cout << "GIT Test" << endl;
+	return 0;
+}
