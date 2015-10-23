34776d95375d2fb9dbb966caf9b6a3a6410b7753
umayat <umayat@requiem.ee.ryerson.ca>
2015-10-23 16:46:51 -0400

    Added comment.

--- Test/src/Test.cpp
+++ Test/src/Test.cpp
@@ -5,7 +5,7 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+//Test
 #include <iostream>
 using namespace std;
 
